09fa5083e048aa60ec15b4d83386c3a039731b5c
scottshull <sshull@students.mchenry.edu>
2016-09-07 08:27:43 -0500

    Add files via upload

---
+++ Wages.py
@@ -0,0 +1,14 @@
+def pay(hours, rate):
+
+    my_pay = rate * hours
+    str_pay = str(my_pay)
+    print("I made " + str_pay + " this paycheck")
+
+
+
+hours = 30
+rate = 12
+
+pay(hours, rate)
+
+pay(30, 12)
